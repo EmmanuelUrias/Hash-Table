581629fc6968e14340964addaae3603b1b929a6c
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-07 09:15:49 -0700

    modifying functionality to avoid collision

--- hash-table.py
+++ hash-table.py
@@ -1,7 +1,7 @@
 class HashTable:
     def __init__(self):
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
 
     def get_hash(self, key):
         h = 0
@@ -9,23 +9,33 @@ class HashTable:
             h += ord(char)
         return h % self.MAX
 
-    def add(self, key, value):
-        h = self.get_hash(key)
-        self.arr[h] = value
+    # def add(self, key, value):
+    #     h = self.get_hash(key)
+    #     self.arr[h] = value
 
     # built in python operator that allows us to get the add functionality with just square brackets
     def __setitem__(self, key, value):
         h = self.get_hash(key)
-        self.arr[h] = value
+        found = False
 
-    def get(self, key):
-        h = self.get_hash(key)
-        return self.arr[h]
+        for index, element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][index] - (key, value)
+                found = True
+                break
+            if not found:
+                self.arr[h].append((key, value))
+
+    # def get(self, key):
+    #     h = self.get_hash(key)
+    #     return self.arr[h]
 
     # built in python operator that allows us to get an item by just putting the key within square brackets
     def __getitem__(self, key):
         h = self.get_hash(key)
-        return self.arr[h]
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
 
     def __delitem__(self, key):
         h = self.get_hash(key)
@@ -33,11 +43,15 @@ class HashTable:
 
 
 t = HashTable()
-t.add('march 10', 205)
+# t.add('march 10', 205)
 new_day = t['march 11'] = 212
+new_day2 = t['march 11'] = 211
 print(new_day)
-print(t.add('march 11', 212))
-print(t['march 10'])
-print(t.get('march 10'))
-del t['march 10']
-print(t['march 10'])
+print(new_day2)
+print(t.get_hash('march 11'))
+# print(t.add('march 11', 212))
+# print(t['march 10'])
+# print(t.get('march 10'))
+# del t['march 10']
+# print(t['march 10'])
+print(t.arr)
